74e314533c3492414509d767c1cbc59d553d3948
elliotlaws <elliotlaws92@gmail.com>
2022-02-24 10:22:35 +0000

    checking cache

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -12,10 +12,18 @@ export default function handleRequest(
     <RemixServer context={remixContext} url={request.url} />
   );
 
+  // Response with status (101, 204, 205, or 304) cannot have a body
+  if ([101, 204, 205, 304].includes(responseStatusCode)) {
+    return new Response(null, {
+      status: responseStatusCode,
+      headers: responseHeaders,
+    });
+  }
+
   responseHeaders.set("Content-Type", "text/html");
 
   return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
-    headers: responseHeaders
+    headers: responseHeaders,
   });
 }
