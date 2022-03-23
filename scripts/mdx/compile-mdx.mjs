7a8b158dab4f3af7521679b0112fc7e1ecbb9dfc
elliotlaws <44065175+elliotlaws@users.noreply.github.com>
2022-03-23 14:06:36 +0000

    Removed unused import from compile-mdx

--- scripts/mdx/compile-mdx.mjs
+++ scripts/mdx/compile-mdx.mjs
@@ -11,7 +11,6 @@ import { getMDXComponent } from "mdx-bundler/client/index.js";
 import rehypeHighlight from "rehype-highlight";
 import { Command } from "commander/esm.mjs";
 import calculateReadTime from "reading-time";
-import { getAverageColor } from "fast-average-color-node";
 
 (async function () {
   config();
