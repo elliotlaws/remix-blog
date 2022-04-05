a47920f8253c2c90f3128a43a7d3d7015af8d4cb
elliotlaws <elliotlaws92@gmail.com>
2022-04-05 21:50:20 +0100

    theme code change

--- app/utils/theme-provider.tsx
+++ app/utils/theme-provider.tsx
@@ -129,11 +129,27 @@ function NonFlashOfWrongThemeEls({ ssrTheme }: { ssrTheme: boolean }) {
   const [theme] = useTheme();
   return (
     <>
+      {/*
+        On the server, "theme" might be `null`, so clientThemeCode ensures that
+        this is correct before hydration.
+      */}
       <meta
         name="color-scheme"
         content={theme === "light" ? "light dark" : "dark light"}
       />
-      <script dangerouslySetInnerHTML={{ __html: clientThemeCode }} />
+      {/*
+        If we know what the theme is from the server then we don't need
+        to do fancy tricks prior to hydration to make things match.
+      */}
+      {ssrTheme ? null : (
+        <script
+          // NOTE: we cannot use type="module" because that automatically makes
+          // the script "defer". That doesn't work for us because we need
+          // this script to run synchronously before the rest of the document
+          // is finished loading.
+          dangerouslySetInnerHTML={{ __html: clientThemeCode }}
+        />
+      )}
     </>
   );
 }
